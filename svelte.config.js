54f82aca81579b5d7a424a33f5ac3b428540504e
Praktikant <praktikant@Praktikants-MacBook-Pro.local>
2024-01-30 09:50:30 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,23 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/sverdle') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
 		}
+	
 	}
 };
 
